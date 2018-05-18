00467126a381336785cd7fac5ed6921413be143b
CIRT Josh <19488645+nogoodconfig@users.noreply.github.com>
2018-05-18 19:32:27 +0100

    Add files via upload

---
+++ Show-ProcessTree.ps1
@@ -0,0 +1,38 @@
+﻿$IdLookup = @{}
+$ParentLookup = @{}
+$Orphans = @()
+
+$Processes = Get-WMIObject -Class Win32_Process
+
+foreach ($Process in $Processes)
+{
+    $IdLookup[$Process.ProcessId] = $Process
+    if (($Process.ParentProcessId -eq 0) -or (!$IdLookup.ContainsKey($Process.ParentProcessId)))
+    {
+        $Orphans += $Process
+        continue
+    }
+
+    if (!$ParentLookup.ContainsKey($Process.ParentProcessId))
+    {
+        $ParentLookup[$Process.ParentProcessId] = @()
+    }
+
+    $Siblings = $ParentLookup[$Process.ParentProcessId]
+    $Siblings += $Process
+    $ParentLookup[$Process.ParentProcessId] = $Siblings
+}
+
+function Show-ProcessTree($ProcessId, $IndentLevel) {
+  $Process = $IdLookup[$ProcessId]
+  $Indent = "-" * $IndentLevel
+
+  Write-Output ("{1}-| {0} PID: {2} PPID: {3}" -f $Process.ProcessName, $Indent, $Process.ProcessId, $Process.ParentProcessId)
+  foreach ($Child in ($ParentLookup[$ProcessId] | Sort-Object CreationDate)) {
+    Show-ProcessTree $Child.ProcessId ($IndentLevel + 1)
+  }
+}
+
+foreach ($Process in ($ProcessesWithoutParents | Sort-Object CreationDate)) {
+  Show-ProcessTree $Process.ProcessId 1
+}
